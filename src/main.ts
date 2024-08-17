1719d84f460b921be4201aeac6f5efbf8a7521e7
Enzo Vignolo <evignolo@cfotechlatam.com>
2024-08-17 09:18:09 -0300

    fix: changing hardcoded port to env variable

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
   console.log(`[OK] API RUNNING ON PORT ${process.env.PORT}`);
   process.on('unhandledRejection', (error, promise) => {
     console.log('Unhandled rejection at ', promise, `reason: ${error}`);
