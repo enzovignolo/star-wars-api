80be80a7ca20b561cf447458b1d3b4630393f99e
Enzo Vignolo <evignolo@cfotechlatam.com>
2024-08-17 09:19:22 -0300

    feat: registering swagger on app module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //register swagger module to app
+  const config = new DocumentBuilder()
+    .setTitle('Star Wars API')
+    .setDescription('The API of Star Wars Movies')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT);
   console.log(`[OK] API RUNNING ON PORT ${process.env.PORT}`);
   process.on('unhandledRejection', (error, promise) => {
