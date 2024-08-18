365327debdee3f15a1ba2d7afa3afc0669067e8f
Enzo Vignolo <evignolo@cfotechlatam.com>
2024-08-18 10:22:10 -0300

    feat: created schema, model and document types for movies

---
+++ src/controllers/movies/schemas/movies.schema.ts
@@ -0,0 +1,26 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Model } from 'mongoose';
+
+@Schema()
+class Movie {
+  @Prop()
+  director: string;
+  @Prop()
+  episodeId: number;
+  @Prop()
+  openingCrawl: string;
+  @Prop()
+  producer: string;
+  @Prop()
+  releaseDate: Date;
+  @Prop()
+  title: string;
+  @Prop({ default: new Date(Date.now()) })
+  createdAt: Date;
+  @Prop({ default: new Date(Date.now()) })
+  updatedAt: Date;
+}
+
+export type MovieDocument = Movie & Document;
+export type MovieModel = Model<Movie>;
+export const MovieSchema = SchemaFactory.createForClass(Movie);
