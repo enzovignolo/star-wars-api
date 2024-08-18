0c2182871fd2da6bf4a3bd30df58a951de857b52
Enzo Vignolo <evignolo@cfotechlatam.com>
2024-08-18 10:20:33 -0300

    feat: created movie entity

---
+++ src/controllers/movies/entities/movies.entity.ts
@@ -0,0 +1,10 @@
+export class Movie {
+  director: string;
+  episodeId: number;
+  openingCrawl: string;
+  producer: string;
+  releaseDate: Date;
+  title: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
