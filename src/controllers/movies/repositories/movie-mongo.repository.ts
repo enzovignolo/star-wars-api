73f43dff695d1fcefbcd5cb2c031cf9a4e7d3380
Enzo Vignolo <evignolo@cfotechlatam.com>
2024-08-18 12:34:01 -0300

    feat: updated MovieMongoRepository

--- src/controllers/movies/repositories/movie-mongo.repository.ts
+++ src/controllers/movies/repositories/movie-mongo.repository.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { Movie } from '../entities/movies.entity';
 import { MoviesRepository } from '../movies.interface';
 import { InjectModel } from '@nestjs/mongoose';
 import { MovieModel } from '../schemas/movies.schema';
+import { CreateMovieDTO } from '../dto/create-movies.dto';
 @Injectable()
 export class MovieMongoRepository implements MoviesRepository {
   constructor(
@@ -11,8 +12,16 @@ export class MovieMongoRepository implements MoviesRepository {
   async getAll(): Promise<Movie[]> {
     return this.movieModel.find();
   }
-  async createOne(): Promise<Movie> {
-    return new Movie();
+  async createOne(createMovieDto: CreateMovieDTO): Promise<Movie> {
+    try {
+      const newMovie = await this.movieModel.create(createMovieDto);
+      return newMovie;
+    } catch (err) {
+      if (err.code) {
+        if (err.code === 11000) throw new BadRequestException(err.errmsg);
+      }
+      return err;
+    }
   }
   async getOne(): Promise<Movie> {
     return new Movie();
@@ -22,5 +31,8 @@ export class MovieMongoRepository implements MoviesRepository {
   }
   async updateOne(): Promise<Movie> {
     return new Movie();
+  }
+  async createBulk(): Promise<void> {
+    return;
   }
 }
