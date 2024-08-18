3f43b74819b7e5792b9fa180bcf2bb82712076bf
Enzo Vignolo <evignolo@cfotechlatam.com>
2024-08-18 10:23:58 -0300

    feat added service, repository and schema to movies module

--- src/controllers/movies/movies.module.ts
+++ src/controllers/movies/movies.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { MoviesController } from './movies.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Movie } from './entities/movies.entity';
+import { MovieSchema } from './schemas/movies.schema';
+import { MoviesService } from './movies.service';
+import { MOVIES_REPOSITORY } from './movies.interface';
+import { MovieMongoRepository } from './repositories/movie-mongo.repository';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forFeature([{ name: Movie.name, schema: MovieSchema }]),
+  ],
   controllers: [MoviesController],
-  providers: [],
+  providers: [
+    MoviesService,
+    { provide: MOVIES_REPOSITORY, useClass: MovieMongoRepository },
+  ],
 })
 export class MoviesModule {}
