9c34ac92eae9930aef3df76c260a2833615ddfff
Enzo Vignolo <evignolo@cfotechlatam.com>
2024-08-18 08:43:00 -0300

    feat: importing MoviesModule on app main module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-
+import { MoviesModule } from './controllers/movies/movies.module';
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), MoviesModule],
   controllers: [AppController],
   providers: [AppService],
 })
