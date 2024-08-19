15548c09d3576e9023a6dd53c61068ab88ff3722
Enzo Vignolo <evignolo@cfotechlatam.com>
2024-08-19 17:12:58 -0300

    feat: registerin auth module to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MoviesModule } from './controllers/movies/movies.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './controllers/auth/auth.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MoviesModule,
+    AuthModule,
     MongooseModule.forRoot(process.env.DB_URI),
   ],
   controllers: [AppController],
