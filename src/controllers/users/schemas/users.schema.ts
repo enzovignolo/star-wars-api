a04fbccd0624bd0f9d53a8f11e799cae200e4d14
Enzo Vignolo <evignolo@cfotechlatam.com>
2024-08-19 17:08:19 -0300

    feat: created user schema

---
+++ src/controllers/users/schemas/users.schema.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Model } from 'mongoose';
+import { ROLES } from 'src/common/constants/role.enum';
+
+@Schema()
+class User {
+  @Prop({ unique: true, required: true })
+  email: string;
+  @Prop({ required: true })
+  hash: string;
+  @Prop({ required: true, default: ROLES.USER })
+  roles: ROLES;
+  @Prop({ default: new Date(Date.now()) })
+  createdAt: Date;
+  @Prop({ default: new Date(Date.now()) })
+  updatedAt: Date;
+}
+
+export type UserDocument = User & Document;
+export type UserModel = Model<User>;
+export const UserSchema = SchemaFactory.createForClass(User);
