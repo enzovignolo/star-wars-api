abd787fc242fe110b2fb49f619e5ed62a2699505
Enzo Vignolo <evignolo@cfotechlatam.com>
2024-08-19 17:57:46 -0300

    feat: registerin JWT module on Auth module

--- src/controllers/auth/auth.module.ts
+++ src/controllers/auth/auth.module.ts
@@ -6,10 +6,17 @@ import { USERS_REPOSITORY } from '../users/users.interface';
 import { AuthController } from './auth.controller';
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { AUTH_CONSTANTS } from 'src/common/constants/auth-constants';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    JwtModule.register({
+      global: true,
+      secret: AUTH_CONSTANTS.JWT_SECRET,
+      signOptions: { expiresIn: AUTH_CONSTANTS.JWT_EXPIRATION_SECONDS },
+    }),
   ],
   controllers: [AuthController],
   providers: [
