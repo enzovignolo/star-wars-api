3ca7d7ea516a52ae98899a6a1dcefa1556da7812
Enzo Vignolo <evignolo@cfotechlatam.com>
2024-08-19 20:22:04 -0300

    fix: correction on status code

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Inject, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Inject,
+  Post,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { SignUpDTO, SignUpResponse } from './dto/signup.dto';
@@ -10,12 +17,14 @@ export class AuthController {
   constructor(@Inject() private readonly authService: AuthService) {}
 
   @ApiOkResponse({ type: SignUpResponse })
+  @HttpCode(HttpStatus.OK)
   @Post('/sign-up')
   async signUp(@Body() data: SignUpDTO): Promise<SignUpResponse> {
     return await this.authService.signUp(data);
   }
 
   @ApiOkResponse({ type: SignInResponse })
+  @HttpCode(HttpStatus.OK)
   @Post('/sign-in')
   async signIn(@Body() data: SignInDTO): Promise<SignInResponse> {
     const token = await this.authService.signIn(data);
