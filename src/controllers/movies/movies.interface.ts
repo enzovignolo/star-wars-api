b143cf62d11a39de1fd8ba2189d19310cb105aac
Enzo Vignolo <evignolo@cfotechlatam.com>
2024-08-19 10:39:06 -0300

    feat: updated repository interface method getOne

--- src/controllers/movies/movies.interface.ts
+++ src/controllers/movies/movies.interface.ts
@@ -1,3 +1,4 @@
+import { Types } from 'mongoose';
 import { CreateMovieDTO } from './dto/create-movies.dto';
 import { Movie } from './entities/movies.entity';
 
@@ -5,7 +6,7 @@ export const MOVIES_REPOSITORY = 'MoviesRepository';
 
 export interface MoviesRepository {
   getAll(): Promise<Movie[]>;
-  getOne(): Promise<Movie>;
+  getOne(id: Types.ObjectId): Promise<Movie>;
   createOne(createMovieDto: CreateMovieDTO): Promise<Movie>;
   updateOne(): Promise<Movie>;
   deleteOne(): Promise<Movie>;
