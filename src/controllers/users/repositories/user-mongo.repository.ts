4947556c2dfea492297c7ee37d4d93651cd533ae
Enzo Vignolo <evignolo@cfotechlatam.com>
2024-08-19 17:09:10 -0300

    feat: created user-mongo repository

---
+++ src/controllers/users/repositories/user-mongo.repository.ts
@@ -0,0 +1,24 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { UsersRepository } from '../users.interface';
+import { UserModel } from '../schemas/users.schema';
+import { User } from '../entities/users.entity';
+import { CreateUserDTO } from '../dto/create-user.dto';
+import { Types } from 'mongoose';
+@Injectable()
+export class UserMongoRepository implements UsersRepository {
+  constructor(@InjectModel(User.name) private readonly userModel: UserModel) {}
+  async createOne(data: CreateUserDTO): Promise<Pick<User, 'id' | 'email'>> {
+    try {
+      const { _id: id, email } = await this.userModel.create(data);
+      return { id, email };
+    } catch (err) {
+      if (err.code) {
+        if (err.code === 11000) throw new BadRequestException(err.errmsg);
+      }
+    }
+  }
+  async getOne(id: Types.ObjectId): Promise<User> {
+    return await this.userModel.findById(id);
+  }
+}
