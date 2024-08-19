68816dd12af3b82ce33ca83160146922a55d4993
Enzo Vignolo <evignolo@cfotechlatam.com>
2024-08-19 20:04:54 -0300

    feat: adding admin user initialization

--- src/controllers/auth/auth.service.ts
+++ src/controllers/auth/auth.service.ts
@@ -2,8 +2,10 @@ import {
   BadRequestException,
   Inject,
   Injectable,
+  InternalServerErrorException,
   NotFoundException,
   UnauthorizedException,
+  UseGuards,
 } from '@nestjs/common';
 import { USERS_REPOSITORY, UsersRepository } from '../users/users.interface';
 import { SignUpDTO } from './dto/signup.dto';
@@ -11,13 +13,34 @@ import * as bcrypt from 'bcryptjs';
 import { SignInDTO } from './dto/signin.dto';
 import { JwtService } from '@nestjs/jwt';
 
+import { ROLES } from 'src/common/constants/role.enum';
+import { ConfigService } from '@nestjs/config';
+
 @Injectable()
 export class AuthService {
   constructor(
     @Inject(USERS_REPOSITORY)
     private readonly usersRepository: UsersRepository,
     private jwtService: JwtService,
+    private configService: ConfigService,
   ) {}
+  /**
+   * initialize ADMIN USER
+   */
+  async onModuleInit() {
+    const email = 'admin@admin.com';
+    if (!(await this.usersRepository.getOneByEmail(email))) {
+      const adminPassword = this.configService.get<string>('ADMIN_PASSWORD');
+      if (!adminPassword)
+        throw new InternalServerErrorException(
+          'Variable ADMIN_PASSWORD has to be defined',
+        );
+      const hash = await bcrypt.hash(adminPassword, 12);
+      await this.usersRepository.createOne({ email, hash, role: ROLES.ADMIN });
+      console.log('ADMIN USER CREATED');
+    }
+  }
+
   async signUp(signUpData: SignUpDTO) {
     if (signUpData.password != signUpData.passwordConfirmation)
       throw new BadRequestException(
