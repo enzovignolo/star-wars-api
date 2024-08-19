4d850c579bea8baed409b7d3e7285e6ca19fff77
Enzo Vignolo <evignolo@cfotechlatam.com>
2024-08-19 17:58:16 -0300

    feat: created POST /sign-in endpoint

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -2,14 +2,22 @@ import { Body, Controller, Inject, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { ApiOkResponse } from '@nestjs/swagger';
 import { SignUpDTO, SignUpResponse } from './dto/signup.dto';
+import { SignInDTO, SignInResponse } from './dto/signin.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(@Inject() private readonly authService: AuthService) {}
 
-  @ApiOkResponse()
+  @ApiOkResponse({ type: SignUpResponse })
   @Post('/sign-up')
   async signUp(@Body() data: SignUpDTO): Promise<SignUpResponse> {
     return await this.authService.signUp(data);
+  }
+
+  @ApiOkResponse({ type: SignInResponse })
+  @Post('/sign-in')
+  async signIn(@Body() data: SignInDTO): Promise<SignInResponse> {
+    const token = await this.authService.signIn(data);
+    return { token };
   }
 }
