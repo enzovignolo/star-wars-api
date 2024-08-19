53b1a4b95fa22d46779297b2c8b53bb4fabf979f
Enzo Vignolo <evignolo@cfotechlatam.com>
2024-08-19 17:57:18 -0300

    feat: added sign in service

--- src/controllers/auth/auth.service.ts
+++ src/controllers/auth/auth.service.ts
@@ -1,13 +1,22 @@
-import { BadRequestException, Inject, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Inject,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { USERS_REPOSITORY, UsersRepository } from '../users/users.interface';
 import { SignUpDTO } from './dto/signup.dto';
 import * as bcrypt from 'bcryptjs';
+import { SignInDTO } from './dto/signin.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @Inject(USERS_REPOSITORY)
     private readonly usersRepository: UsersRepository,
+    private jwtService: JwtService,
   ) {}
   async signUp(signUpData: SignUpDTO) {
     if (signUpData.password != signUpData.passwordConfirmation)
@@ -20,5 +29,14 @@ export class AuthService {
       hash,
     });
     return userCreated;
+  }
+  async signIn(signInData: SignInDTO): Promise<string> {
+    const user = await this.usersRepository.getOneByEmail(signInData.email);
+    if (!user || !(await bcrypt.compare(signInData.password, user.hash)))
+      throw new UnauthorizedException('Email or password incorrect');
+    return await this.jwtService.signAsync({
+      email: user.email,
+      role: user.role,
+    });
   }
 }
